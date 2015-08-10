91ec6c6b7106960293eefe5795a7cfc11f905daf
Stefan Bauer <stefan.bauer@n8d.at>
2015-08-10 08:23:56 +0200

    Final Burger menu

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,2 +1,45 @@
 // jshint devel:true
-console.log('\'Allo \'Allo!');
+var polyfillBurger = polyfillBurger || {};
+
+(function($) {
+    
+    'use strict';
+
+    polyfillBurger = function(element) {
+
+        // show burger event definition
+        var showHideBurger = function(event) {
+            event.preventDefault();
+            
+            if ($(this).hasClass('showBurger')) {
+                $(this).removeClass('showBurger');
+            
+            } else {
+                $(this).addClass('showBurger');
+            
+            }
+        };
+
+        // request element with burger
+        var burger = $(element);
+
+        // check if burger exists
+        if (burger !== undefined &&
+            burger.length !== 0) {
+
+            // register burger menu
+            burger.on('click', showHideBurger);
+            burger.each(function(index, el) {
+                $(el).on('click', showHideBurger);
+            });
+
+        }
+
+    };
+
+
+    $(document).ready(function() {
+        polyfillBurger('.ms-core-listMenu-horizontalBox');
+    });
+
+}(jQuery));
